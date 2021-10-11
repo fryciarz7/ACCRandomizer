658269425ea855d4c38ddc35a71f16957bb9cecc
fryciarz7 <fryciarz7@gmail.com>
2021-10-11 12:05:52 +0200

    added Car model

---
+++ Model/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ACCRandomizer.Model
+{
+    public class Car
+    {
+        public string CarName { get; set; }
+        public Enums.Enums.CarClassEnum CarClass { get; set; }
+        public string CarImage { get; set; }
+    }
+}
