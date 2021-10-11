86a01f5005ac680eb546485bd663719c05528fde
fryciarz7 <fryciarz7@gmail.com>
2021-10-11 10:45:38 +0200

    added enums

--- Enums/CarClass.cs
+++ Enums/CarClass.cs
@@ -7,5 +7,13 @@ namespace ACCRandomizer.Enums
 {
     public class CarClass
     {
+        private enum CarClasses
+        {
+            Mixed = 0,
+            Cup = 1,
+            ST = 2,
+            GT3 = 3,
+            GT4 = 4
+        }
     }
 }
